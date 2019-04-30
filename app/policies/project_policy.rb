8282b83aec96c4265bb0793c9cd0c5aaa1de20ec
DEVSINC399 <ertiza.ejaz@devsinc.com>
2019-04-30 15:21:15 +0500

    Proejc t Policy Atomic

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -5,15 +5,24 @@ class ProjectPolicy < ApplicationPolicy
     end
   end
 
-  def new? ; @user.role.name == 'Manager' || @user.role.name == 'Admin' ; end
-  def create? ; @user.role.name == 'Manager' || @user.role.name == 'Admin' ; end
-  def show? ; is_allowed? || @record.users.where(:id => @user.id).present? ; end
+  def new? ; is_manager? || is_admin? ; end
+  def create? ; is_manager? || is_admin? ; end
+  def show? ; is_allowed? || is_project_user? ; end
   def update? ; is_allowed? ; end
   def destroy? ; is_allowed? ; end
 
   private
   def is_allowed?
     @user.id == @record.created_by || @user.role.name == 'Admin'
+  end
+  def is_project_user
+    @record.users.where(:id => @user.id).present?
+  end
+  def is_manager?
+    @user.role.name == 'Manager'
+  end
+  def is_admin?
+    @user.role.name == 'Admin'
   end
 
 end
